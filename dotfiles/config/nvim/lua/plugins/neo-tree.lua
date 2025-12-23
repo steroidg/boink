539857ac3211f9dd2cc6bf103b4d16735e0c9f79
steroidg <steroidg@gmail.com>
2025-12-23 22:32:50 +1100

    fix: moved web-devicons into its own config area

--- dotfiles/config/nvim/lua/plugins/neo-tree.lua
+++ dotfiles/config/nvim/lua/plugins/neo-tree.lua
@@ -5,7 +5,6 @@ return {
     dependencies = {
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
-      "nvim-tree/nvim-web-devicons", -- optional, but recommended
     },
 
     lazy = false, -- neo-tree will lazily load itself
