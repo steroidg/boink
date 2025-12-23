4cd9aa909f534b590fe1a051de455f2248e57b99
steroidg <steroidg@gmail.com>
2025-12-23 22:30:26 +1100

    feat: added snaks config

---
+++ dotfiles/config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    ---@type snacks.Config
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      -- bigfile = { enabled = true },
+      -- dashboard = { enabled = true },
+      -- explorer = { enabled = true },
+      indent = {
+        indent = {
+          enabled = false,
+        },
+        chunk = {
+          enabled = true,
+          char = {
+            horizontal = '─',
+            vertical = '│',
+            corner_top = '╭',
+            corner_bottom = '╰',
+            arrow = '─',
+          },
+        },
+      },
+      -- input = { enabled = true },
+      -- picker = { enabled = true },
+      -- notifier = { enabled = true },
+      -- quickfile = { enabled = true },
+      -- scope = { enabled = true },
+      -- scroll = { enabled = true },
+      -- statuscolumn = { enabled = true },
+      -- words = { enabled = true },
+    },
+  }
+}
