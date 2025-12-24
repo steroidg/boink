67057933a6b7cce0679aa089ecb54e25daf251be
steroidg <steroidg@gmail.com>
2025-12-24 11:39:35 +1100

    feat: added ability to close tab and window when there's only neotree open

--- dotfiles/config/nvim/lua/plugins/neo-tree.lua
+++ dotfiles/config/nvim/lua/plugins/neo-tree.lua
@@ -14,8 +14,40 @@ return {
       vim.api.nvim_create_autocmd("VimEnter", {
         callback = function()
           -- Only open Neo-tree if we're not opening a specific file (optional)
-          if vim.fn.argc() == 0 then
-            vim.cmd("Neotree show")
+          --if vim.fn.argc() == 0 then
+          vim.cmd("Neotree show")
+          --end
+        end,
+      })
+
+      -- Close tab if the file is closed but neotree panel is visible
+      vim.api.nvim_create_autocmd("BufEnter", {
+        group = vim.api.nvim_create_augroup("NeoTreeCloseTab", { clear = true }),
+        callback = function()
+          local tabs = vim.api.nvim_list_tabpages()
+          if #tabs > 1 then
+            local wins = vim.api.nvim_tabpage_list_wins(0)
+            -- If only 1 window left and it's Neo-tree, close the tab
+            if #wins == 1 then
+              local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(wins[1]))
+              if bufname:match("neo%-tree") then
+                vim.cmd("tabclose")
+              end
+            end
+          end
+        end,
+      })
+
+      -- Quit neovim if the file is closed but neotree panel is visible
+      vim.api.nvim_create_autocmd("BufEnter", {
+        group = vim.api.nvim_create_augroup("NeoTreeQuit", { clear = true }),
+        callback = function()
+          local wins = vim.api.nvim_list_wins()
+          if #wins == 1 then
+            local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(wins[1]))
+            if bufname:match("neo%-tree") then
+              vim.cmd("quit")
+            end
           end
         end,
       })
