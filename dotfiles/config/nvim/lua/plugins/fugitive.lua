b067787ab81876087c5f713af341bbb0345dd674
steroidg <steroidg@gmail.com>
2025-12-24 11:59:38 +1100

    feat: added some key mappings for fugitive

--- dotfiles/config/nvim/lua/plugins/fugitive.lua
+++ dotfiles/config/nvim/lua/plugins/fugitive.lua
@@ -1,5 +1,24 @@
 return {
   {
     "tpope/vim-fugitive",
+    config = function()
+      -- Toggle Git Status (The main Fugitive interface)
+      vim.keymap.set("n", "<leader>g", function()
+        if vim.bo.filetype == "fugitive" then
+          vim.cmd("close")
+        else
+          vim.cmd("Git")
+        end
+      end, { desc = "Toggle Fugitive Status" })
+
+      -- Git Commit
+      vim.keymap.set("n", "<leader>gm", "<cmd>Git commit<cr>", { desc = "Git Commit" })
+
+      -- Git Push
+      vim.keymap.set("n", "<leader>gp", "<cmd>Git push<cr>", { desc = "Git Push" })
+
+      -- Toggle Git Diff Split (Vertical)
+      vim.keymap.set("n", "<leader>gd", "<cmd>Gdiffsplit!<cr>", { desc = "Git Diff Split" })
+    end,
   },
 }
